c62aa9a06338132418af5a9378d0350b70d0055c
James Barrow <jamb@sitecore.net>
2021-03-18 11:27:02 +1100

    Initial commit

---
+++ Malian.SitecoreExtensions.AzureApplicationInsights/Malian.Foundation.AzureExtensions/Pipelines/InjectAdditionalTelemetryInitialisers.cs
@@ -0,0 +1,28 @@
+﻿using Malian.Foundation.AzureExtensions.TelemetryInitialisers;
+using Microsoft.ApplicationInsights.Extensibility;
+using Sitecore.Configuration;
+using Sitecore.Pipelines;
+using System;
+
+namespace Malian.Foundation.AzureExtensions.Pipelines
+{
+    public class InjectAdditionalTelemetryInitialisers
+    {
+        public virtual void Process(PipelineArgs args)
+        {
+            string roleName = Settings.GetSetting("ApplicationInsights.RoleName", string.Empty);
+
+            if (string.IsNullOrEmpty(roleName))
+            {
+                roleName = Environment.GetEnvironmentVariable("WEBSITE_SITE_NAME");
+            }
+
+            if (string.IsNullOrEmpty(roleName))
+            {
+                return;
+            }
+
+            TelemetryConfiguration.Active.TelemetryInitializers.Add(new CloudRoleNameInitialiser(roleName));
+        }
+    }
+}
---
+++ Malian.SitecoreExtensions.AzureApplicationInsights/Malian.Foundation.AzureExtensions/TelemetryInitialisers/CloudRoleNameInitialiser.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.ApplicationInsights.Channel;
+using Microsoft.ApplicationInsights.Extensibility;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Malian.Foundation.AzureExtensions.TelemetryInitialisers
+{
+    public class CloudRoleNameInitialiser : ITelemetryInitializer
+    {
+        private readonly string roleName;
+
+        public CloudRoleNameInitialiser(string roleName)
+        {
+            this.roleName = roleName ?? throw new ArgumentNullException(nameof(roleName));
+        }
+
+        public void Initialize(ITelemetry telemetry)
+        {
+            telemetry.Context.Cloud.RoleName = this.roleName;
+        }
+    }
+}
